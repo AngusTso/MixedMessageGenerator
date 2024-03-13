037a7ff9681797a893eca07509d94be118ad1378
AngusTso <angusking47@gmail.com>
2024-03-13 17:48:08 +0800

    Updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const message = [
-    'Keep going , you almost there',
-    'Work hard and smart',
-    'Just do it',
-    'You are the best',
-    'You can do it',
+const subject = [
+    'I',
+    'You',
+    'He',
+    'She',
+    'It',
 ]
 
+const verb = [
+    'Love',
+    'Hate',
+    'Kill',
+    'Kiss',
+    'Punch'
+]
+
+const object = [
+    'You',
+    'Your father',
+    'Your mother',
+    'Your Sister',
+    'Him'
+]
+function main(){
+    let randSubject = Math.floor(Math.random() * 5);
+    let randVerb = Math.floor(Math.random() * 5);
+    let randObject = Math.floor(Math.random() * 5);
+    console.log(`${subject[randSubject]} ${verb[randVerb]} ${object[randObject]}`);
+}
+
+main();
